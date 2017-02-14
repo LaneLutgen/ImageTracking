68430442c2aa4e82ed3e98a36e98aaeaf783fa20
Lane Lutgen <lutgenlane@gmail.com>
2017-02-14 15:12:09 -0700

    Added initial files

---
+++ src/Main.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 14, 2017
+
+@author: Lane
+'''
+
+import numpy as num
+import cv2
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Video")
+while True:
+    status, img = cap.read()
+    cv2.imshow("Video", img)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+cv2.destroyAllWindows()
