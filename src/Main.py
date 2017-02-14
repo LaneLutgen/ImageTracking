c2e29fd57915b83d7f297ee33d0bb6f8ed50e024
Lane Lutgen <lutgenlane@gmail.com>
2017-02-14 15:52:17 -0700

    Set mouse callback function

--- src/Main.py
+++ src/Main.py
@@ -7,8 +7,28 @@ Created on Feb 14, 2017
 import numpy as num
 import cv2
 
+
+x_coor = 0
+y_coor = 0
+    
+def mouseCall(evt, x, y, flags, pic):
+    global x_coor
+    global y_coor
+    if evt == cv2.EVENT_LBUTTONDOWN:
+        print("Coordinates set")
+        x_coor = x
+        y_coor = y
+        print("X: %d" % x_coor)
+        print("Y: %d" % y_coor)
+        
+        
+#Main
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("Video")
+cv2.namedWindow("HSV")
+
+#Set mouse callback function
+cv2.setMouseCallback("HSV", mouseCall, None)
 
 while True:
     #Read the frame
