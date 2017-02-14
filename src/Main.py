8f2715695a63bacf11983d7f36357374a0d5d906
Lane Lutgen <lutgenlane@gmail.com>
2017-02-14 15:24:14 -0700

    Added HSV window, changed quit button to q

--- src/Main.py
+++ src/Main.py
@@ -6,12 +6,23 @@ Created on Feb 14, 2017
 
 import numpy as num
 import cv2
+
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("Video")
+
 while True:
+    #Read the frame
     status, img = cap.read()
+    
+    #Convert from BGR to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    #Show all the windows
     cv2.imshow("Video", img)
-    k = cv2.waitKey(1)
-    if k == 27:
+    cv2.imshow("HSV", hsv)
+    
+    #Press q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
 cv2.destroyAllWindows()
