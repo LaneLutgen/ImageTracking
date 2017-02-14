72182ec4a998366a1f68fd5e46e2627b6dbb2045
Lane Lutgen <lutgenlane@gmail.com>
2017-02-14 16:22:44 -0700

    Added the code for grabbing the HSV value from the mouse click

--- src/Main.py
+++ src/Main.py
@@ -4,12 +4,15 @@ Created on Feb 14, 2017
 @author: Lane
 '''
 
-import numpy as num
+import numpy as np
 import cv2
 
 
 x_coor = 0
 y_coor = 0
+height = 0
+width = 0
+hsvValue = np.uint8([0,0,0]) 
     
 def mouseCall(evt, x, y, flags, pic):
     global x_coor
@@ -20,6 +23,7 @@ def mouseCall(evt, x, y, flags, pic):
         y_coor = y
         print("X: %d" % x_coor)
         print("Y: %d" % y_coor)
+        print(hsvValue)
         
         
 #Main
@@ -40,6 +44,9 @@ while True:
     #Show all the windows
     cv2.imshow("Video", img)
     cv2.imshow("HSV", hsv)
+    
+    if x_coor != 0 and y_coor != 0:
+        hsvValue = hsv[y_coor, x_coor]
     
     #Press q to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
